7f186d436c9ae6ab15981cdc2cd87a817db6b3d7
ybmadhu <ybmadhu707@gmail.com>
2017-04-25 15:11:51 +0530

    changed in sop statement

--- App.java
+++ App.java
@@ -2,7 +2,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World ..!" );
+        System.out.println( "Hello World .. madhu sudhan reddy!" );
     }
 
 	public int calculateSomething() {
