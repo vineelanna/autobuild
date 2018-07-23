f78d601a930fe4e87a846fe1af9cf40c9ddfa0b5
Vineel Annabathula <31035002+vineelanna@users.noreply.github.com>
2018-07-23 14:35:15 +0530

    Added my name

--- App.java
+++ App.java
@@ -2,7 +2,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World .. JMS technologies." );
+        System.out.println( "Hello World .. This is Vineel. This Works !!" );
     }
 
 	public int calculateSomething() {
